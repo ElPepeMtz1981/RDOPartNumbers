44d72e0ba0b90be5fe83f0d9e24b73d6d6b97a91
ElPepeMtz1981 <elpepemtz@gmail.com>
2025-07-01 18:47:27 -0600

    Enviroment variables validation

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using PartNumbers.Data;
 using PartNumbers.Middleware;
 
@@ -14,11 +14,28 @@ builder.Services.AddCors(options =>
     });
 });
 
+builder.Configuration
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true)
+    .AddEnvironmentVariables();
 
 builder.Services.AddControllers();
 builder.Services.AddDbContext<PartNumbersDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("PartNumbers")));
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var connection = builder.Configuration.GetConnectionString("PartNumbers");
+if (string.IsNullOrWhiteSpace(connection) || connection.Contains("USE_ENV_VARIABLE"))
+{
+    builder.Logging.AddConsole();
+    Console.WriteLine("⚠️  La cadena de conexión no fue cargada desde el entorno.");
+}
+else
+{
+    Console.WriteLine($"✅ Conexión detectada desde entorno: {connection.Split(';')[0]}");
+}
+
 
 var app = builder.Build();
 
